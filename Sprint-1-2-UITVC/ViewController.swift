cf8a0abb0023c95fd83db32fffcb6cfa0c4de2e4
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-13 10:03:05 -0500

    Initial Commit

---
+++ Sprint-1-2-UITVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sprint-1-2-UITVC
+//
+//  Created by Ilgar Ilyasov on 10/13/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
